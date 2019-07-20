fd9945efdc0080e04fc6c0891cbfef00412efff1
Ben Orgera <benorg@me.com>
2019-07-20 16:39:37 -0400

    Wallets working with trades and transfers

--- Wallet.js
+++ Wallet.js
@@ -1,5 +1,11 @@
 const as = require('./ArraySet');
 
+// wallet is a map from coins and exchanges to holdings by exchange and coin respectively, where each
+// holding (coin/exchange) pair can have multiple pebbles
+// the invariant is maintained that:
+//   as.get(getHoldingsInCoin(wallet, coin), exchangeId) === as.get(getHoldingsInExchange(wallet, exchangeId), coin)
+//   so the holding (metadata + pebbles list) is shared by both lists of holdings in the maps indexed by coin/exchange
+
 function empty() {
 	return {
 		coinMap: new Map(),
@@ -8,37 +14,42 @@ function empty() {
 }
 
 // requires wallet has pebble, private method
-function removeHolding(wallet, pebble) {
+function removePebble(wallet, pebble) {
 	const { exchangeId, coin, amount, pebbleId } = pebble,
-	      holdingsInCoin = getHoldingsInCoin(wallet, pebble.coin),
-	      holdingsInExchange = getHoldingsInCoin(wallet, pebble.exchangeId);
+	      holdingsInCoin = getHoldingsInCoin(wallet, coin), // could have lookup up by exchange too
+	      holding = as.get(holdingsInCoin, exchangeId);
 
-	holdingsInCoin.amount -= amount;
-	holdingsInExchange.amount -= amount;
+	holding.amount -= amount;
 
-	as.remove(holdingsInCoin.pebbles, pebbleId);
-	as.remove(holdingsInExchange.pebbles, pebbleId);
+	as.remove(holding.pebbles, pebbleId);
+
+	if (holding.pebbles.length === 0) { // last pebble for this coin/exchangeId pair
+		as.remove(holdingsInCoin, exchangeId);
+		as.remove(getHoldingsInExchange(wallet, exchangeId), coin);
+	} else if (holding.amount <= 0)
+	    throw new Error(`Negative holding in ${exchangeId}:${coin} after trade or transfer`);
 }
 
-function addHolding(wallet, pebble) {
+function addPebble(wallet, pebble) {
 
 	const { exchangeId, coin, amount, pebbleId } = pebble;
 
 	if (!wallet.coinMap.has(coin))
-		wallet.coinMap.set(coin, as.empty(holding => holding.exchangeId)); // each coin has an ArraySet indexed by exchange where its holdings are stored
+		wallet.coinMap.set(coin, as.empty(holding => holding.exchangeId)); // each coin has an ArraySet indexed by exchange where its holdings (pebbles + metadata) are stored
 
 	if (!wallet.exchangeMap.has(exchangeId))
-		wallet.exchangeMap.set(exchangeId, as.empty(holding => holding.coin)); // each exchange has an ArraySet indexed by coin where its holdings are stored
+		wallet.exchangeMap.set(exchangeId, as.empty(holding => holding.coin)); // each exchange has an ArraySet indexed by coin where its holdings (pebbles + metadata) are stored
 
 	const coinArray = wallet.coinMap.get(coin),
 	      exchangeArray = wallet.exchangeMap.get(exchangeId),
 	      lookup = as.get(coinArray, exchangeId); // lookup in ArraySet for given coin by exchange (equivalent to as.get(exchangeArray, coin) because these have same elements)
 
 	if (lookup) {
-		lookup.totalAmount += amount;
+		lookup.amount += amount;
+
 	    as.add(lookup.pebbles, pebble);
 	} else {
-		const pebbles = as.empty(pebble => pebbleId),
+		const pebbles = as.empty(pebble => pebble.pebbleId),
 		      holding = { coin, exchangeId, amount, pebbles };
 
 		as.add(pebbles, pebble);
@@ -49,24 +60,28 @@ function addHolding(wallet, pebble) {
 }
 
 // requires pebble is in wallet
+// requires endCoinPerStart > 0
 // has side effect of actually changing passed in pebble
 function tradePebble(wallet, pebble, endCoin, endCoinPerStartCoin) {
-	removeHolding(wallet, pebble);
+	removePebble(wallet, pebble);
 
 	pebble.coin = endCoin;
 	pebble.amount *= endCoinPerStartCoin;
 
-	addHolding(wallet, pebble);
+	addPebble(wallet, pebble);
 }
 
 // requires pebble is in wallet
-function transferPebble(wallet, pebble, transferFee, endExchangeId) {
-	removeHolding(wallet, pebble);
+function transferPebble(wallet, pebble, endExchangeId, transferFee) {
+	removePebble(wallet, pebble);
 
 	pebble.exchangeId = endExchangeId;
-	pebble.amount -= fee;
+	pebble.amount -= transferFee;
 
-	addHolding(wallet, pebble);
+	if (pebble.amount <= 0)
+		throw new Error(`Transfer fee on ${exchangeId}:${coin} greater than holding itself`);
+
+	addPebble(wallet, pebble);
 }
 
 
@@ -78,4 +93,4 @@ function getHoldingsInExchange(wallet, exchangeId) {
 	return wallet.exchangeMap.get(exchangeId) || as.empty(holding => holding.coin);
 }
 
-module.exports = { empty, addHolding, tradePebble, transferPebble, getHoldingsInExchange, getHoldingsInCoin };
+module.exports = { empty, addPebble, tradePebble, transferPebble, getHoldingsInExchange, getHoldingsInCoin };
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const gr = require('./Graph'),
       util = require('./Util'),
-      wt = require('./Wallet');
+      wt = require('./Wallet'),
+      uuid = require('uuid/v4');
 
 
 var G = gr.newGraph();
@@ -41,30 +42,81 @@ console.log('3-cycles from BTC: ' + JSON.stringify(gr.getAllNCyclesFromS(G, 3, [
 
 
 
+const wallet = wt.empty(),
+      peb = {
+      	coin: 'BTC',
+      	amount: 2,
+      	exchangeId: 'bittrex',
+      	pebbleId: uuid()
+      };
 
-var wallet = wt.empty();
-
-wt.addHolding(wallet, 'bittrex', 'BTC', 2);
+wt.addPebble(wallet, peb);
 
 console.log('BTC holdings (2 bittrex): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'BTC'), null, 4));
 console.log('bittrex holdings (2 BTC): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'bittrex'), null, 4));
 
-wt.addHolding(wallet, 'bittrex', 'ETH', 3);
+const peb1 = {
+      	coin: 'ETH',
+      	amount: 3,
+      	exchangeId: 'bittrex',
+      	pebbleId: uuid()
+      };
+
+wt.addPebble(wallet, peb1);
 
 console.log('BTC holdings (2 bittrex): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'BTC'), null, 4));
 console.log('bittrex holdings (2 BTC, 3 ETH): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'bittrex'), null, 4));
 
-wt.addHolding(wallet, 'bittrex', 'BTC', 0.5);
+const peb2 = {
+      	coin: 'BTC',
+      	amount: 0.5,
+      	exchangeId: 'bittrex',
+      	pebbleId: uuid()
+      };
+
+wt.addPebble(wallet, peb2);
 
 console.log('BTC holdings (2.5 bittrex): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'BTC'), null, 4));
 console.log('bittrex holdings (2.5 BTC, 3 ETH): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'bittrex'), null, 4));
 
-wt.addHolding(wallet, 'binance', 'BTC', 1);
+const peb3 = {
+      	coin: 'BTC',
+      	amount: 1,
+      	exchangeId: 'binance',
+      	pebbleId: uuid()
+      };
+
+wt.addPebble(wallet, peb3);
 
 console.log('BTC holdings (2.5 bittrex, 1 binance): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'BTC'), null, 4));
 console.log('bittrex holdings (2.5 BTC, 3 ETH): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'bittrex'), null, 4));
 console.log('binace holdings (1 BTC): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'binance'), null, 4));
 
+
+wt.tradePebble(wallet, peb2, 'ETH', 2); // trade 0.5 BTC on BTX to 1 ETH
+
+console.log('BTC holdings (2 bittrex, 1 binance): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'BTC'), null, 4));
+console.log('ETH holdings (4 bittrex): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'ETH'), null, 4));
+console.log('bittrex holdings (2 BTC, 4 ETH): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'bittrex'), null, 4));
+console.log('binace holdings (1 BTC): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'binance'), null, 4));
+
+wt.tradePebble(wallet, peb3, 'LTC', 3); // trade 1 BTC on binance to 3 LTC
+
+console.log('BTC holdings (2 bittrex): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'BTC'), null, 4));
+console.log('binace holdings (3 LTC): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'binance'), null, 4));
+console.log('LTC holdings (3 binance): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'LTC'), null, 4));
+
+wt.transferPebble(wallet, peb, 'binance', 0.1); // transfer 2 BTC on btx to 1.9 on binance
+
+console.log('BTC holdings (1.9 binance): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'BTC'), null, 4));
+console.log('bittrex holdings (4 eth): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'bittrex'), null, 4));
+console.log('binace holdings (3 LTC, 1.9 BTC): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'binance'), null, 4));
+
+wt.transferPebble(wallet, peb2, 'binance', 0.2); // transfer 1 ETH on btx to 0.8 on binance
+
+console.log('ETH holdings (3 bittrex, 0.8 binance): ' + JSON.stringify(wt.getHoldingsInCoin(wallet, 'ETH'), null, 4));
+console.log('bittrex holdings (3 eth): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'bittrex'), null, 4));
+console.log('binace holdings (3 LTC, 1.9 BTC, 0.8 ETH): ' + JSON.stringify(wt.getHoldingsInExchange(wallet, 'binance'), null, 4));
 
 require('./Snapshots').takeSnapshots();
 
